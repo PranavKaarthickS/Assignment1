fad70524e5c3d2d8ee5b3ea885a0c560708b8f85
PranavKaarthickS <98024988+PranavKaarthickS@users.noreply.github.com>
2022-01-19 17:28:31 +0530

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,12 @@
+var_int = 15
+var_folat = 18.5
+var_Boolean = "true"
+var_List = [1,2,3,4,5,6,7,8,9,10]
+var_tuple = tuple()
+var_dict = dict()
+print(var_int)
+print(var_folat)
+print(var_Boolean)
+print(var_List)
+print(var_tuple)
+print(var_dict)
